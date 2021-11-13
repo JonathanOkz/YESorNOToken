ab37b78494b7c007cce781e04c2ca60228b2979e
Jonathan <jonathan@med.pics>
2021-11-13 02:12:17 +0100

    improve security by reviewing the roles management

--- contracts/lib/vesting/VestingSingleRevocable.sol
+++ contracts/lib/vesting/VestingSingleRevocable.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "./VestingSingle.sol";
 
 /****************************
@@ -12,14 +13,22 @@ import "./VestingSingle.sol";
 * by the owner if the employee resigns before the end of the acquisition period.
 ******/
 
-contract VestingSingleRevocable is VestingSingle {
+contract VestingSingleRevocable is VestingSingle, AccessControlEnumerable {
     using SafeERC20 for IERC20;
+
+    bytes32 public constant REVOKER_ROLE = keccak256("REVOKER_ROLE");
 
     event Revoked();
 
     bool private _revoked;
 
-    constructor (IERC20 token, address beneficiary, uint delayInDay, uint durationInDay) VestingSingle(token, beneficiary, delayInDay, durationInDay) {}
+    /**
+     * @notice
+     * Grants `REVOKER_ROLE` to the account `revoker`.
+     */
+    constructor (IERC20 token, address beneficiary, uint delayInDay, uint durationInDay, address revoker) VestingSingle(token, beneficiary, delayInDay, durationInDay) {
+        _setupRole(REVOKER_ROLE, revoker);
+    }
 
     /****************************
      * Getters
@@ -44,15 +53,15 @@ contract VestingSingleRevocable is VestingSingle {
      *
      * Requirements:
      *
-     * - the caller is the owner.
+     * - the caller must have the `REVOKER_ROLE`.
      * - the contract is not yet revoked.
      * - the amount of TOKEN locked is greater than 0.
      */
-    function revoke() external nonReentrant onlyOwner {
-        require(_revoked != true, "VestingSingleRevocable: is already revoked");
+    function revoke() external nonReentrant {
+        require(hasRole(REVOKER_ROLE, _msgSender()), "VestingSingleRevocable: the caller is not authorized");
+        require(_revoked != true,                    "VestingSingleRevocable: is already revoked");
 
         uint256 locked = getLocked();
-
         require(locked > 0, "VestingSingleRevocable: no tokens to burn");
 
         (bool success,) = address(_token).call(abi.encodeWithSignature("burn(uint256)", locked));
