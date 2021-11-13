b423723925a93f82c46f6e16c96ba93467744775
Jonathan <jonathan@med.pics>
2021-11-13 01:41:57 +0100

    use native constants for time duration

--- contracts/lib/vesting/release/VestingReleaseLinear.sol
+++ contracts/lib/vesting/release/VestingReleaseLinear.sol
@@ -11,20 +11,23 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract VestingReleaseLinear {
     using SafeMath for uint256;
 
-    uint private constant SECOND = 1;
-    uint private constant MINUTE = 60 * SECOND;
-    uint private constant HOUR   = 60 * MINUTE;
-    uint private constant DAY    = 24 * HOUR;
-
     uint private immutable _start;
     uint private immutable _duration;
 
+    /**
+     * @notice
+     *
+     * Requirements:
+     *
+     * - `delayInDay` must be greater (or equal) than 0 and less than 3651.
+     * - `durationInDay` must be greater (or equal) than 0 and less than 3651.
+     */
     constructor (uint delayInDay, uint durationInDay) {
-        require(delayInDay >= 0 && delayInDay <= 3650,      "VestingReleaseLinear: delayInDay must be greater (or equal) than 0 and less than 3651");
+        require(delayInDay >= 0 && delayInDay <= 3650,       "VestingReleaseLinear: delayInDay must be greater (or equal) than 0 and less than 3651");
         require(durationInDay >= 0 && durationInDay <= 3650, "VestingReleaseLinear: durationInDay must be greater (or equal) than 0 and less than 3651");
 
-        _start = block.timestamp.add(delayInDay.mul(DAY));
-        _duration = durationInDay.mul(DAY);
+        _start = block.timestamp.add(delayInDay.mul(1 days));
+        _duration = durationInDay.mul(1 days);
     }
 
     /**
@@ -58,7 +61,11 @@ contract VestingReleaseLinear {
     }
 
     /**
-     * @return Calculate the linear amount releasable.
+     * @return The linear amount releasable.
+     *
+     * Requirements:
+     *
+     * - `_duration` must be greater than 0.
      */
     function _calculateLinearAmountReleasable(uint256 vested, uint256 released) private view returns (uint256) {
         require(_duration > 0, "VestingReleaseLinear: _duration must be greater than 0");
