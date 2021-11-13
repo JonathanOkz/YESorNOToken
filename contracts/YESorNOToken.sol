bf2292253c46439bf083db632628dea5a06e8da3
Jonathan <jonathan@med.pics>
2021-11-13 01:48:38 +0100

    chnage name of YESorNOToken

--- contracts/YESorNOToken.sol
+++ contracts/YESorNOToken.sol
@@ -5,7 +5,7 @@ import "./lib/Token.sol";
 
 contract YESorNOToken is Token {
 
-    string  private constant _name   = "YESorNO";
+    string  private constant _name   = "YES||NO";
     string  private constant _symbol = "YON";
     uint256 private constant _cap    = 9000000000 * 10**18;
 
