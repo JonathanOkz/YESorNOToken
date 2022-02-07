90e2ff78a958a8b1f5a29b0e86e8fb2954715f94
Jonathan <jonathan@med.pics>
2022-02-07 20:19:57 +0100

    add optimizer and change bsc testnet

--- truffle-config.js
+++ truffle-config.js
@@ -44,13 +44,13 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    // bsc_testnet: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-    //   network_id: 97,
-    //   confirmations: 10,
-    //   timeoutBlocks: 200,
-    //   skipDryRun: true
-    // },
+    bsctest: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     bsc: {
       provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed.binance.org`),
       network_id: 56,
@@ -103,13 +103,13 @@ module.exports = {
     solc: {
       version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 2**32-1
+       },
       //  evmVersion: "byzantium"
-      // }
+      }
     }
   },
 
