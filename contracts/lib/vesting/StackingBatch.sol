97b93080aa413217a9b6a0437a69fbc0686037b6
Jonathan <jonathan@med.pics>
2022-02-13 17:49:27 +0100

    refactoring and adding some comments

--- contracts/lib/vesting/StackingBatch.sol
+++ contracts/lib/vesting/StackingBatch.sol
@@ -7,57 +7,58 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /****************************
 * 
-* 
+* This contract blocks for a stacking period an amount of TOKENs for a stacker.
+* Each stacker via his public address can only participate once to this program.
+* When he participates, a portion of the reward pool is allocated to him, until
+* it is empty. When the reward pool is empty, access to the stacking program is
+* no longer possible. To accept new participants, it will be necessary to add
+* TOKENs to the reward pool.
+*
 ******/
 
 contract StackingBatch is ReentrancyGuard {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-
-    event Released(address indexed stacker, uint256 amount);
-    event Joined(address indexed stacker, uint256 amount);
-    event PoolIncreased(uint256 amount);
+    event Released(address stacker, uint256 amount);
+    event Joined(address stacker, uint256 amount, uint256 giftAmount, bool giftNoAds, bool giftExclusiveAvantage);
+    event PoolIncreased(address user, uint256 amount, uint256 pool);
 
     uint256 SMALL  = 150000 * 10**18;  // 150K
     uint256 MEDIUM = 500000 * 10**18;  // 500K
     uint256 LARGE  = 1500000 * 10**18; // 1.5M
 
-    struct Gift {
+    struct Gift_t {
         uint256 amount;
-        bool noAds;
-        bool freeExclusiveBadge;
+        bool    noAds;
+        bool    exclusiveAvantage;
     }
-    struct Stacker {
-        uint    unlockDate;
+    struct Stacker_t {
+        uint256 unlockDate;
         uint256 amount;
-        Gift    gift;
+        Gift_t  gift;
         bool    released;
     }
-
-    mapping(address => Stacker) private _map;
-    address[] private _array;
+    mapping(address => Stacker_t) private _map;
 
     uint256 private _pool;
 
-    IERC20 private immutable _token;
-
-    uint private immutable _delayInDay;
+    uint256 private immutable _delayInDay;
+    IERC20  private immutable _token;
 
     /**
      * @notice
-     * Define the TOKEN IERC20 interface for the stacking program.
-     *
-     * Grants `REWARDER_ROLE` to the account `rewarder`.
+     * Define the TOKEN IERC20 and the delay in day for the stacking program.
      *
      * - `delayInDay` must be greater (or equal) than 0 and less than 3651.
      */
-    constructor (IERC20 token, uint delayInDay) {
+    constructor (IERC20 token, uint256 delayInDay) {
         require(delayInDay >= 0 && delayInDay <= 3650, "StackingBatch: delayInDay must be greater (or equal) than 0 and less than 3651");
 
         _delayInDay = delayInDay;
         _token = token;
     }
+    
 
     /****************************
      * Getters
@@ -67,87 +68,72 @@ contract StackingBatch is ReentrancyGuard {
      * @return the token.
      */
     function getToken() external view returns (IERC20) {
-        return _token;
+        return _getToken();
     }
 
     /**
-     * @return the total amount stacked.
-     */
-    function getTotalStacked() external view returns (uint256) {
-        return _getTotalStacked();
-    }
-
-    /**
-     * @return the total amount releasable.
-     */
-    function getTotalReleasable() external view returns (uint256) {
-        return _getTotalReleasable();
-    }
-
-    /**
-     * @return the total amount released.
-     */
-    function getTotalReleased() external view returns (uint256) {
-        return _getTotalReleased();
-    }
-
-
-    /**
-     * @return the total amount of reward pool.
+     * @return the total amount of the reward pool.
      */
     function getPool() external view returns (uint256) {
         return _getPool();
     }
 
     /**
-     * @return return the amount stacked for a the `msg.sender`.
-     */
-    function getReleasable() external view returns (uint256) {
-        return _getReleasable(msg.sender);
-    }
-
-    /**
-     * @return return the amount stacked for a the `msg.sender`.
+     * @return the stacked amount for the caller.
      */
     function getStacked() external view returns (uint256) {
         return _getStacked(msg.sender);
     }
 
     /**
-     * @return return the amount stacked for a the `msg.sender`.
+     * @return the the gift amount and the stacked amount for the caller.
+     */
+    function getReleasable() external view returns (uint256) {
+        return _getReleasable(msg.sender);
+    }
+
+    /**
+     * @return the the gift amount for the caller.
      */
     function getGiftAmount() external view returns (uint256) {
         return _getGiftAmount(msg.sender);
     }
 
+    /**
+     * @return if the caller is eligible for the "no ads" gift.
+     */
     function getGiftNoAds() external view returns (bool) {
         return _getGiftNoAds(msg.sender);
     }
 
-    function getGiftFreeExclusiveBadge() external view returns (bool) {
-        return _getGiftFreeExclusiveBadge(msg.sender);
+    /**
+     * @return if the caller is eligible for the "exclusive avantage" gift.
+     */
+    function getGiftExclusiveAvantage() external view returns (bool) {
+        return _getGiftExclusiveAvantage(msg.sender);
     }
 
     /**
-     * @return the start date for the `msg.sender`.
+     * @return the date when the caller can unlock the stacked amount.
      */
     function getUnlockDate() external view returns (uint) {
         return _getUnlockDate(msg.sender);
     }
 
     /**
-     * @return if the `msg.sender` is in the stacking program.
+     * @return if the caller is in the stacking program.
      */
     function exist() external view returns (bool) {
         return _exist(msg.sender);
     }
 
     /**
-     * @return if the `msg.sender` has released.
+     * @return if the caller has released.
      */
     function released() external view returns (bool) {
         return _released(msg.sender);
     }
+
 
     /****************************
      * Public functions
@@ -163,9 +149,11 @@ contract StackingBatch is ReentrancyGuard {
      */
     function increaseRewardPool(uint256 amount) external nonReentrant {
         require(amount > 0, "StackingBatch: amount must be > 0");
-        _token.safeTransferFrom(msg.sender, address(this), amount);
+
+        _getToken().safeTransferFrom(msg.sender, address(this), amount);
         _increasePool(amount);
-        emit PoolIncreased(_getPool());
+
+        emit PoolIncreased(msg.sender, amount, _getPool());
     }
 
     /**
@@ -174,16 +162,21 @@ contract StackingBatch is ReentrancyGuard {
      * Requirements:
      *
      * - the `amount` must be in one of the available programs.
-     * - the caller must have called TOKEN.approve function before.
      * - the `stacker` is not yet in the stacking program.
      * - the reward pool must be sufficient to accept a new `stacker`.
+     * - the caller must have called TOKEN.approve function before.
      */
     function join(uint256 amount) external nonReentrant {
         require(amount == SMALL || amount == MEDIUM || amount == LARGE, "StackingBatch: amount is unsuitable");
-        _token.safeTransferFrom(msg.sender, address(this), amount);
-        _append(msg.sender, amount);
-        // require(_token.balanceOf(address(this)) >= _getTotalStacked() + _getPool(), "StackingBatch: no enough TOKEN available !");
-        emit Joined(msg.sender, amount);
+        require(!_exist(msg.sender), "StackingBatch: stacker already exist");
+
+        Gift_t memory gift = _generateGift(amount);
+        require(_getPool() >= gift.amount, "StackingBatch: no enough tokens available for gift");
+
+        _getToken().safeTransferFrom(msg.sender, address(this), amount);
+        _append(msg.sender, block.timestamp.add(_delayInDay.mul(1 days)), amount, gift);
+        
+        emit Joined(msg.sender, amount, gift.amount, gift.noAds, gift.exclusiveAvantage);
     }
 
     /**
@@ -194,8 +187,16 @@ contract StackingBatch is ReentrancyGuard {
      * - the caller is in the stacking program and has any amount of TOKEN realesable.
      */
     function release() external nonReentrant {
-        uint256 releasable = _release(msg.sender);
-        _token.safeTransfer(msg.sender, releasable);
+        require(_exist(msg.sender), "StackingBatch: stacker not exist");
+        require(block.timestamp > _getUnlockDate(msg.sender), "StackingBatch: tokens are not releasable for now");
+        require(!_released(msg.sender), "StackingBatch: stacker has already released these TOKENs");
+
+        uint256 releasable = _getReleasable(msg.sender);
+        require(releasable > 0, "StackingBatch: no tokens to release");
+
+        _map[msg.sender].released = true;
+        _getToken().safeTransfer(msg.sender, releasable);
+
         emit Released(msg.sender, releasable);
     }
 
@@ -203,80 +204,33 @@ contract StackingBatch is ReentrancyGuard {
      * Private functions
      ******/
 
-    function _append(address stacker, uint256 amount) private {
-        require(!_exist(msg.sender), "StackingBatch: stacker already exist");
-        Gift memory gift = _generateGift(amount);
-        require(_getPool() >= gift.amount, "StackingBatch: no enough tokens available for gift !");
-        _map[stacker] = Stacker(block.timestamp.add(_delayInDay.mul(1 days)), amount, gift, false);
-        _array.push(stacker);
+    function _append(address stacker, uint256 unlockDate, uint256 amount, Gift_t memory gift) private {
+        _map[stacker] = Stacker_t(unlockDate, amount, gift, false);
         _decreasePool(gift.amount);
     }
 
-    function _release(address stacker) private returns (uint256) {
-        require(_exist(stacker), "StackingBatch: stacker not exist");
-        require(_getUnlockDate(stacker) < block.timestamp, "StackingBatch: tokens are not releasable for now");
-        require(!_released(stacker), "StackingBatch: stacker has already released these TOKENs");
-
-        uint256 releasable = _getReleasable(stacker);
-        require(releasable > 0, "StackingBatch: no tokens to release");
-
-        _map[stacker].released = true;
-
-        return releasable;
-    }
-
-    function _generateGift(uint256 amount) private view returns (Gift memory) {
+    function _generateGift(uint256 amount) private view returns (Gift_t memory) {
         if (amount == SMALL) {
             // 7% rewarded
             // No Ads for ever
-            return Gift(amount.div(100).mul(7), true, false);
+            return Gift_t(amount.div(100).mul(7), true, false);
         } else if (amount == MEDIUM) {
             // 11% rewarded
             // No Ads for ever
-            return Gift(amount.div(100).mul(11), true, false);
+            return Gift_t(amount.div(100).mul(11), true, false);
         } else if (amount == LARGE) {
             // 15% rewarded
             // No Ads for ever
             // 1 NFT free
-            return Gift(amount.div(100).mul(15), true, true);
+            return Gift_t(amount.div(100).mul(15), true, true);
         } else {
-            return Gift(0, false, false);
+            return Gift_t(0, false, false);
         }
     }
 
-    // total
-
-    function _getTotalStacked() private view returns (uint256) {
-        uint256 total = 0;
-        for (uint256 i = 0; i < _array.length; i++) {
-            if (_map[_array[i]].released == false) {
-                total += _map[_array[i]].amount;
-            }
-        }
-        return total;
+    function _getToken() private view returns (IERC20) {
+        return _token;
     }
-
-    function _getTotalReleased() private view returns (uint256) {
-        uint256 total = 0;
-        for (uint256 i = 0; i < _array.length; i++) {
-            if (_map[_array[i]].released == true) {
-                total += _map[_array[i]].amount;
-            }
-        }
-        return total;
-    }
-
-    function _getTotalReleasable() private view returns (uint256) {
-        uint256 total = 0;
-        for (uint256 i = 0; i < _array.length; i++) {
-            if (_map[_array[i]].released == false) {
-                total += _map[_array[i]].amount + _map[_array[i]].gift.amount;
-            }
-        }
-        return total;
-    }
-
-    // pool
 
     function _getPool() private view returns (uint256) {
         return _pool;
@@ -289,8 +243,6 @@ contract StackingBatch is ReentrancyGuard {
     function _decreasePool(uint256 amount) private {
         _pool = _pool.sub(amount);
     }
-
-    // stacker
 
     function _exist(address stacker) private view returns (bool) {
         return _map[stacker].amount > 0;
@@ -308,10 +260,7 @@ contract StackingBatch is ReentrancyGuard {
     }
 
     function _getStacked(address stacker) private view returns (uint256) {
-        if (!_released(stacker)) {
-            return _map[stacker].amount;
-        }
-        return 0;
+        return _map[stacker].amount;
     }
 
     function _getGiftAmount(address stacker) private view returns (uint256) {
@@ -322,8 +271,8 @@ contract StackingBatch is ReentrancyGuard {
         return _map[stacker].gift.noAds;
     }
 
-    function _getGiftFreeExclusiveBadge(address stacker) private view returns (bool) {
-        return _map[stacker].gift.freeExclusiveBadge;
+    function _getGiftExclusiveAvantage(address stacker) private view returns (bool) {
+        return _map[stacker].gift.exclusiveAvantage;
     }
 
     function _getUnlockDate(address stacker) private view returns (uint) {
